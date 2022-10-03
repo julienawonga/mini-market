e990d83edf4b7af5161f9b1b2bd95e7a28fa7824
Julien AWON'GA <julienawon@gmail.com>
2022-10-03 22:09:13 +0100

    Add header

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {ChakraProvider} from '@chakra-ui/react';
+import {Grid, GridItem} from '@chakra-ui/react';
+import Header from './Components/Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider>
+          <Grid
+      templateAreas={`"header header"
+                      "nav main"
+                      "nav footer"`}
+      gridTemplateRows={'90px 1fr 90px'}
+      gridTemplateColumns={'300px 1fr'}
+      h='100vh'
+      gap='1'
+      color='blackAlpha.700'
+      fontWeight='bold'
+    >
+      <GridItem pl='0' bg='orange.300' area={'header'}>
+        <Header />
+      </GridItem>
+      <GridItem pl='10%' pr='10%' bg='pink.300' area={'nav'}>
+        Nav
+      </GridItem>
+      <GridItem pl='10%' pr='10%' bg='green.300' area={'main'}>
+        Main
+      </GridItem>
+      <GridItem pl='10%' pr='10%' bg='blue.300' area={'footer'}>
+        Footer
+      </GridItem>
+    </Grid>
+    </ChakraProvider>
+   
   );
 }
 
